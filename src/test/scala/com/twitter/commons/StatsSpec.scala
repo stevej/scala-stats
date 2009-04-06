9e14bc379d1f387be4b81862c0031aaa485eddb1
Steve Jenson <stevej@pobox.com>
2009-04-05 20:44:03 -0700

    first external release of scala-stats. work on scala-stats will be done in this repo.

---
+++ src/test/scala/com/twitter/commons/StatsSpec.scala
@@ -0,0 +1,15 @@
+/** Copyright 2009 Twitter, Inc. */
+package com.twitter.commons
+
+import net.lag.extensions._
+import org.specs._
+import scala.collection.immutable
+
+
+object StatsSpec extends Specification {
+  "Stats" should {
+    "add 2 numbers" in {
+      1 + 1 mustEqual 2
+    }
+  }
+}
