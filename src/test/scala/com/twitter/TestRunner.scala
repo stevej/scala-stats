b29e792c9105bdc53b28426c2e07b6dd21fb4402
Robey Pointer <robey@twitter.com>
2009-05-05 16:50:53 +0800

    upgrade dependencies
    Signed-off-by: Steve Jenson <stevej@pobox.com>

--- src/test/scala/com/twitter/TestRunner.scala
+++ src/test/scala/com/twitter/TestRunner.scala
@@ -3,8 +3,10 @@ package com.twitter.service
 
 import net.lag.configgy.Configgy
 import net.lag.logging.Logger
+import org.specs.runner.SpecsFileRunner
 
-object TestRunner extends FilterableSpecsFileRunner("src/test/scala/com/twitter/**/*.scala") {
+object TestRunner extends SpecsFileRunner("src/test/scala/**/*.scala", ".*",
+  System.getProperty("system", ".*"), System.getProperty("example", ".*")) {
   if (System.getProperty("debugtrace") == null) {
     Logger.get("").setLevel(Logger.OFF)
   }
