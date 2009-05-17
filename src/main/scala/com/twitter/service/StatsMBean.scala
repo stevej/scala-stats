28d5015bb544e4d8ac8df7debe566fe512ff3f55
Robey Pointer <robey@twitter.com>
2009-05-17 14:53:48 -0700

    add jmx support, and some friggin' tests. (what happened to the old tests?)

---
+++ src/main/scala/com/twitter/service/StatsMBean.scala
@@ -0,0 +1,81 @@
+/*
+* Copyright 2009 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.service
+
+import java.lang.management.ManagementFactory
+import javax.{management => jmx}
+
+
+object StatsMBean {
+  def apply(packageName: String): Unit = apply(packageName, ManagementFactory.getPlatformMBeanServer())
+
+  def apply(packageName: String, mbeanServer: jmx.MBeanServer): Unit = {
+    mbeanServer.registerMBean(new StatsMBean(), new jmx.ObjectName(packageName + ":type=Stats"))
+  }
+}
+
+
+class StatsMBean extends jmx.DynamicMBean {
+  def getMBeanInfo() = {
+    new jmx.MBeanInfo("com.twitter.service.Stats", "running statistics", getAttributeInfo(),
+      null, null, null, new jmx.ImmutableDescriptor("immutableInfo=false"))
+  }
+
+  def getAttribute(name: String): AnyRef = {
+    val segments = name.split("_", 2)
+    segments(0) match {
+      case "counter" =>
+        Stats.getCounterStats()(segments(1)).asInstanceOf[java.lang.Long]
+      case "timing" =>
+        val prefix = segments(1).split("_", 2)
+        val timing = Stats.getTimingStats(false)(prefix(1))
+        val x = prefix(0) match {
+          case "min" => timing.minimum
+          case "max" => timing.maximum
+          case "count" => timing.count
+          case "average" => timing.average
+        }
+        x.asInstanceOf[java.lang.Integer]
+      case "gauge" =>
+        Stats.getGaugeStats(false)(segments(1)).asInstanceOf[java.lang.Double]
+    }
+  }
+
+  def getAttributes(names: Array[String]): jmx.AttributeList = {
+    val rv = new jmx.AttributeList
+    for (name <- names) rv.add(new jmx.Attribute(name, getAttribute(name)))
+    rv
+  }
+
+  def invoke(actionName: String, params: Array[Object], signature: Array[String]): AnyRef = throw new IllegalStateException()
+
+  def setAttribute(attr: jmx.Attribute): Unit = throw new IllegalStateException()
+
+  def setAttributes(attrs: jmx.AttributeList): jmx.AttributeList = throw new IllegalStateException()
+
+  private def getAttributeInfo(): Array[jmx.MBeanAttributeInfo] = {
+    (Stats.getCounterStats().keys.map { name =>
+      List(new jmx.MBeanAttributeInfo("counter_" + name, "java.lang.Long", "counter", true, false, false))
+    } ++ Stats.getTimingStats(false).keys.map { name =>
+      List("min", "max", "average", "count") map { prefix =>
+        new jmx.MBeanAttributeInfo("timing_" + prefix + "_" + name, "java.lang.Integer", "timing", true, false, false)
+      }
+    } ++ Stats.getGaugeStats(false).keys.map { name =>
+      List(new jmx.MBeanAttributeInfo("gauge_" + name, "java.lang.Long", "gauge", true, false, false))
+    }).toList.flatten[jmx.MBeanAttributeInfo].toArray
+  }
+}
--- src/test/scala/com/twitter/service/StatsSpec.scala
+++ src/test/scala/com/twitter/service/StatsSpec.scala
@@ -8,8 +8,57 @@ import scala.collection.immutable
 
 object StatsSpec extends Specification {
   "Stats" should {
-    "be embarassed at how few tests there are for this class." in {
-      1 + 1 mustEqual 2
+    doBefore {
+      Stats.clearAll()
+    }
+
+    "report jvm stats" in {
+      val jvmStats = Stats.getJvmStats()
+      jvmStats.keys.toList must contain("num_cpus")
+      jvmStats.keys.toList must contain("heap_used")
+      jvmStats.keys.toList must contain("start_time")
+    }
+
+    "report counters" in {
+      Stats.incr("widgets", 1)
+      Stats.incr("wodgets", 12)
+      Stats.incr("wodgets")
+      Stats.getCounterStats() mustEqual Map("widgets" -> 1, "wodgets" -> 13)
+    }
+
+    "report timings" in {
+      var x = 0
+      Stats.time("hundred") { for (i <- 0 until 100) x += i }
+      val timings = Stats.getTimingStats(false)
+      timings.keys.toList mustEqual List("hundred")
+      timings("hundred").count mustEqual 1
+      timings("hundred").minimum mustEqual timings("hundred").average
+      timings("hundred").maximum mustEqual timings("hundred").average
+    }
+
+    "reset timings when asked" in {
+      var x = 0
+      Stats.time("hundred") { for (i <- 0 until 100) x += i }
+      Stats.getTimingStats(false)("hundred").count mustEqual 1
+      Stats.time("hundred") { for (i <- 0 until 100) x += i }
+      Stats.getTimingStats(false)("hundred").count mustEqual 2
+      Stats.getTimingStats(true)("hundred").count mustEqual 2
+      Stats.time("hundred") { for (i <- 0 until 100) x += i }
+      Stats.getTimingStats(false)("hundred").count mustEqual 1
+    }
+
+    "report gauges" in {
+      Stats.makeGauge("pi") { java.lang.Math.PI }
+      Stats.getGaugeStats(false) mustEqual Map("pi" -> java.lang.Math.PI)
+    }
+
+    "report to JMX" in {
+      Stats.incr("widgets", 1)
+      Stats.time("nothing") { 2 * 2 }
+
+      val mbean = new StatsMBean
+      val names = mbean.getMBeanInfo().getAttributes().toList.map { _.getName() }
+      names mustEqual List("counter_widgets", "timing_min_nothing", "timing_max_nothing", "timing_average_nothing", "timing_count_nothing")
     }
   }
 }
