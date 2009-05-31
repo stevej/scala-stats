92b5f9b4b5cecd30e41735d1ad2f1acfd0fa030a
Steve Jenson <stevej@pobox.com>
2009-05-31 15:07:49 -0700

    adding a convenience method on top of Timing

--- src/main/scala/com/twitter/commons/Stats.scala
+++ src/main/scala/com/twitter/commons/Stats.scala
@@ -71,6 +71,15 @@ object Stats {
     }
 
     /**
+     * Evaluates function f, returning value of type T, and logging the duration in the current Timer instance.
+     */
+    def time[T](f: => T): T = {
+      val (rv, duration) = Stats.time(f)
+      add(duration.asInstanceOf[Int]) // is safe as long as it didn't take longer then 46 days.
+      rv
+    }
+
+    /**
      * Returns a tuple of (Count, Min, Max, Average) for the measured event.
      * If `reset` is true, it clears the current event timings also.
      */
