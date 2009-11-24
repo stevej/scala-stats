3f4ae41a698b32180a6ec85c282f825681c98ef3
Robey Pointer <robey@twitter.com>
2009-11-23 17:34:47 -0800

    bump version.

--- src/test/scala/com/twitter/stats/TestRunner.scala
+++ src/test/scala/com/twitter/stats/TestRunner.scala
@@ -18,11 +18,6 @@ package com.twitter.stats
 
 import net.lag.configgy.Configgy
 import net.lag.logging.Logger
-import org.specs.runner.SpecsFileRunner
+import com.twitter.xrayspecs.XraySpecsRunner
 
-object TestRunner extends SpecsFileRunner("src/test/scala/**/*.scala", ".*",
-  System.getProperty("system", ".*"), System.getProperty("example", ".*")) {
-  if (System.getProperty("debugtrace") == null) {
-    Logger.get("").setLevel(Logger.OFF)
-  }
-}
+object TestRunner extends XraySpecsRunner
